a37403fa8322d72b875447ec4b045648dff0ff99
gabisenaa <136934244+gabisenaa@users.noreply.github.com>
2025-11-10 13:33:51 -0300

    Setup completo: estrutura base RADIORUM MVP

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const Page = () => <div>Welcome to RADIORUM MVP</div>;
+export default Page;
---
+++ config/.eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = { parser: '@typescript-eslint/parser', extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'], settings: { react: { version: 'detect' }, }, rules: { // your eslint rules here } };
